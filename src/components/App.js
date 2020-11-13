f2172d55a452d2dc42f71e5665925373ee075852
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-11-13 16:37:04 +0530

    chngs

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  render() {
+    let relatives = ["Abcd", "Efgh", "Ijkl", "Mnop", "Qrst"];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
